9e7d8506a3f87d12dc25356dd9a591ae7aa6be1f
devt-10 <dev.thakkar@learner.manipal.edu>
2023-12-31 21:08:33 +0530

    tailwind + countdown + button text alignment

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,9 +5,35 @@ export const Home = () => {
   return (
     <div className="home">
       <div>
-        <h1>AmongUs</h1>
+        <h1 className="text-8xl">AmongUs</h1>
       </div>
-      <div> - MIT Exclusive Hackathon!</div>
+      <div className="text-6xl">MIT Exclusive Hackathon!</div>
+      <div className="grid grid-flow-col gap-5 text-center auto-cols-max">
+        <div className="flex flex-col">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 15 }}></span>
+          </span>
+          days
+        </div>
+        <div className="flex flex-col">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 10 }}></span>
+          </span>
+          hours
+        </div>
+        <div className="flex flex-col">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 24 }}></span>
+          </span>
+          min
+        </div>
+        <div className="flex flex-col">
+          <span className="countdown font-mono text-5xl">
+            <span style={{ "--value": 42 }}></span>
+          </span>
+          sec
+        </div>
+      </div>
     </div>
   );
 };
