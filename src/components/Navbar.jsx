7c1e09b72155daea8790c33aaa3636a3e951b369
devt-10 <dev.thakkar@learner.manipal.edu>
2024-01-06 01:02:32 +0530

    devfolio logo and assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,21 @@ import React, { useState } from "react";
 
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apply.devfolio.co/v2/sdk.js";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <nav className="flex justify-center items-center sticky top-0 h-32 sm:justify-between relative">
@@ -33,6 +45,15 @@ export const Navbar = () => {
         </li>
         <li>
           <NavLink
+            to="/contact"
+            className="button"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            Contact
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
             to="/Register"
             className="button"
             onClick={() => setMenuOpen(!menuOpen)}
@@ -41,15 +62,25 @@ export const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink
-            to="/contact"
-            className="button"
-            onClick={() => setMenuOpen(!menuOpen)}
-          >
-            Contact
-          </NavLink>
+          <img
+            src="src\assets\devfolio_dark.png"
+            alt="DEVFOLIO LOGO"
+            srcset=""
+            className="h-[2.6rem] mr-1 mb-1"
+          />
         </li>
       </ul>
     </nav>
   );
 };
+
+// .button {
+//     padding: 0.3rem 1rem;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     border: 2px solid #ffffffd5;
+//     border-radius: 6px;
+//     font-size: 2rem;
+//     height: 2.5rem;
+// }
